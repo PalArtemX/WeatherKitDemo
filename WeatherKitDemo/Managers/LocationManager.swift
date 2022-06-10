3a81c7e0bad3e94a44e3e368ee234da26bca3731
Artem Paliutin <pal.artem@icloud.com>
2022-06-10 15:58:16 +0300

    added LocationManager

---
+++ WeatherKitDemo/Managers/LocationManager.swift
@@ -0,0 +1,44 @@
+//
+//  LocationManager.swift
+//  WeatherKitDemo
+//
+//  Created by Artem Paliutin on 10/06/2022.
+//
+
+import Foundation
+import CoreLocation
+
+
+class LocationManager: NSObject, ObservableObject {
+    @Published var currentLocation: CLLocation?
+    
+    private let locationManager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        locationManager.delegate = self
+    }
+    
+    
+}
+
+
+
+
+
+// MARK: - Extension LocationManager
+extension LocationManager: CLLocationManagerDelegate {
+
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last, currentLocation == nil else { return }
+        
+        DispatchQueue.main.async {
+            self.currentLocation = location
+        }
+    }
+}
