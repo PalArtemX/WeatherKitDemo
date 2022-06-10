0bd54da9ba84fef7320a6530a252f531801f2b10
Artem Paliutin <pal.artem@icloud.com>
2022-06-10 15:44:48 +0300

    start

---
+++ WeatherKitDemo/Views/MainView.swift
@@ -0,0 +1,29 @@
+//
+//  MainView.swift
+//  WeatherKitDemo
+//
+//  Created by Artem Paliutin on 10/06/2022.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
--- WeatherKitDemo/WeatherKitDemoApp.swift
+++ WeatherKitDemo/WeatherKitDemoApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct WeatherKitDemoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
     }
 }
