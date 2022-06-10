d3466e1b2b12b202cbeacffa92429b060a4ec34e
Artem Paliutin <pal.artem@icloud.com>
2022-06-10 15:41:07 +0300

    Initial Commit

---
+++ WeatherKitDemo/WeatherKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherKitDemoApp.swift
+//  WeatherKitDemo
+//
+//  Created by Artem Paliutin on 10/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
